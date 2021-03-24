2bf647e6a8964a75a3790393d4d8fc03c46897e3
Nitesh Karki <niteshkarki1998@gmail.com>
2021-03-24 15:54:54 +0545

    third commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,8 @@ function NavBar() {
   return (
     <>
 
-      <div className="container1">
-        <div className="row">
+      <div className="container-fluid">
+        <div className="row align-items-center" style={{ backgroundColor: '#131921' }}>
           <div className="col-md-8">
 
             <form className="d-flex p-3 col-md-8">
@@ -19,30 +19,33 @@ function NavBar() {
           </div>
           <div className="col-md-4">
             <ul className="d-flex p-3">
-              <li className="list-unstyled"><Link className="text-decoration-none p-3 "  style={{color: "white"}} to="/Signup">Signup</Link></li>
-              <li className="list-unstyled"><Link className="text-decoration-none p-3"  style={{color: "white"}} to="/Signin">Signin</Link></li>
+              <li className="list-unstyled"><Link className="text-decoration-none p-3" style={{ fontSize: '20px', color: 'white' }} to="/Signup"><i class="bi bi-person-plus-fill" style={{ color: 'orange', fontSize: '20px' }}></i>&nbsp;Signup</Link></li>
+              <li className="list-unstyled"><Link className="text-decoration-none p-3" style={{ fontSize: '20px', color: 'white' }} to="/Signin"><i class="bi bi-box-arrow-in-right" style={{ color: 'orange', fontSize: '20px' }}></i>&nbsp;Signin</Link></li>
+              <li className="list-unstyled"><Link className="text-decoration-none p-3"><i class="bi bi-cart-check-fill" style={{ color: 'orange', fontSize: '25px' }}></i>&nbsp;<sup style={{ backgroundColor: 'blue', borderRadius: '50%', padding: '3px', color: 'white', fontSize: '20px' }}>100</sup></Link></li>
+
+
             </ul>
           </div>
         </div>
       </div>
 
 
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: '#232f3e' }}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">E-Commerce</Link>
+          <Link className="navbar-brand text-white" to="/">E-Commerce</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active text-white" aria-current="page" to="/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/offer">Offer</Link>
+                <Link className="nav-link text-white" to="/offer">Offer</Link>
               </li>
               <li className="nav-item dropdown">
-                <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <Link className="nav-link dropdown-toggle text-white" to="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                   Dropdown</Link>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li><Link className="dropdown-item" to="#">Action</Link></li>
@@ -50,9 +53,6 @@ function NavBar() {
                   <li><hr className="dropdown-divider" /></li>
                   <li><Link className="dropdown-item" to="#">Something else here</Link></li>
                 </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link disabled" to="#" tabindex="-1" aria-disabled="true">Disabled</Link>
               </li>
             </ul>
 
